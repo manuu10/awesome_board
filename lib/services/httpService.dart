9617788272b35e301becc1566fb0ca96a7ac2fb9
manuu10 <manuel.alberding@web.de>
2021-03-30 01:01:55 +0200

    version upadted

--- lib/services/httpService.dart
+++ lib/services/httpService.dart
@@ -4,10 +4,11 @@ import 'dart:io' as io;
 
 class HttpService {
   static final String _host = "https://xn--blleblle-n4ae.de";
-  static int _versionNumber = 5;
+  static int _versionNumber = 6;
 
   static Future<bool> updateAvailable() async {
-    http.Response response = await http.get(_host + "/install/climbingboard/version.txt");
+    http.Response response =
+        await http.get(_host + "/install/climbingboard/version.txt");
     int version = int.tryParse(response.body) ?? -1;
     if (version > _versionNumber) return true;
 
@@ -15,7 +16,8 @@ class HttpService {
   }
 
   static Future<String> refreshWallImage() async {
-    var response = await http.get(_host + "/install/climbingboard/custom_moonboard.png");
+    var response =
+        await http.get(_host + "/install/climbingboard/custom_moonboard.png");
     if (response.statusCode >= 400) return "error";
     var dir = await getApplicationDocumentsDirectory();
     var file = io.File(dir.path + "/moon.png");
